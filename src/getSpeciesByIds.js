77a7310b7439e948ee5eeaf880088607572b8369
Ludson <ludson_ps27@hotmail.com>
2022-06-29 16:41:12 -0300

    Requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui.
+// console.log(data.species);
+
+function getSpeciesByIds(...ids) {
+  if (ids) {
+    const resultado = [];
+    for (let index = 0; index < ids.length; index += 1) {
+      resultado.push(data.species.find((element) => element.id === ids[index]));
+    }
+    return resultado;
+  } return [];
 }
+
+console.log(getSpeciesByIds(
+  '0938aa23-f153-4937-9f88-4858b24d6bce',
+  'e8481c1d-42ea-4610-8e11-1752cfc05a46',
+));
 
 module.exports = getSpeciesByIds;
