c1283672d1d239f93b3d8be4b726774c348e8319
Ludson <ludson_ps27@hotmail.com>
2022-06-30 11:08:15 -0300

    Requisito 6 e 7

--- src/handlerElephants.js
+++ src/handlerElephants.js
@@ -2,6 +2,7 @@ const { species } = require('../data/zoo_data');
 
 const getElephants = () =>
   species.find((specie) => specie.name === 'elephants');
+  console.log(getElephants());
 
 const averageAge = ({ residents }) =>
   residents.reduce((sum, elephant) => sum + elephant.age, 0) / residents.length;
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,19 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Verificando se ao passar nenhum valor retorna undefined', () => {
+    expect(handlerElephants()).toBeUndefined();
+  });
+  it('Verificando se ao passar um parâmetro que não é uma string ele retorna mensagem de erro', () => {
+    expect(handlerElephants(1)).toBe('Parâmetro inválido, é necessário uma string');
+  });
+  it('Verificando se ao passarmos a string \'count\' o que retorna', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+  it('Verificando se ao passarmos a string \'names\' o que retorna', () => {
+    expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
+  });
+  it('Verificando se ao passarmos a string \'averageAge\' o que retorna', () => {
+    expect(handlerElephants('averageAge')).toEqual(10.5);
+  });
+});
