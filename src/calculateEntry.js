782c0280603ffbc8897ca709227fc9a2ba5b1374
Ludson <ludson_ps27@hotmail.com>
2022-06-30 14:47:48 -0300

    Requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,30 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const child = entrants.filter((element) => element.age < 18);
+  const adult = entrants.filter((element) => element.age >= 18 && element.age < 50);
+  const senior = entrants.filter((element) => element.age >= 50);
+  return {
+    child: child.length,
+    adult: adult.length,
+    senior: senior.length,
+  };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined) {
+    return 0;
+  }
+  const objeto = Object.keys(entrants).length;
+  if (objeto === 0) {
+    return 0;
+  }
+  const result = countEntrants(entrants);
+  const valorChild = result.child * 20.99;
+  const valorAdult = result.adult * 49.99;
+  const valorSenior = result.senior * 24.99;
+  const valorTotal = valorChild + valorAdult + valorSenior;
+  return valorTotal;
 }
 
 module.exports = { calculateEntry, countEntrants };
