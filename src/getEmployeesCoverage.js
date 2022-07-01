d3a1f9047c6daf2b4922c1fde045b0aefb77b337
Ludson <ludson_ps27@hotmail.com>
2022-07-01 14:02:23 -0300

    Requisito 12 e 13

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,89 +1,26 @@
-const data = require('../data/zoo_data');
-// function species
-const functionSpecies = (arr) => {
-  const arrTeste = [];
-  arr.forEach((e) => {
-    for (let i = 0; i < data.species.length; i += 1) {
-      // console.log(`Esse é meu e: ${e}`);
-      // console.log(`Esse é meu id: ${data.species[i].id}`);
-      if (e === data.species[i].id) {
-        arrTeste.push(data.species[i].name);
-      }
-    }
-  });
-  return arrTeste;
-};
-
-// função para retornar o location
-const functionLocation = (arr) => {
-  const arrTeste = [];
-  arr.forEach((e) => {
-    for (let i = 0; i < data.species.length; i += 1) {
-      // console.log(`Esse é meu e: ${e}`);
-      // console.log(`Esse é meu id: ${data.species[i].id}`);
-      if (e === data.species[i].id) {
-        arrTeste.push(data.species[i].location);
-      }
-    }
-  });
-  return arrTeste;
-};
-
-// função para diminuir linhas
-const testeFunction = (id, firstName, lastName, responsibleFor) => {
-  return {
-    id,
-    fullName: `${firstName} ${lastName}`,
-    species: functionSpecies(responsibleFor),
-    locations: functionLocation(responsibleFor),
-  };
-};
-
-//  função para todos
-const todos = (responsibleFor) => {
-  const arrTeste = [];
-  data.employees.forEach((e) => {
-    arrTeste.push({
-      id: e.id,
-      fullName: `${e.firstName} ${e.lastName}`,
-      species: functionSpecies(e.responsibleFor),
-      locations: functionLocation(e.responsibleFor),
-    });
-  });
-  return arrTeste;
-};
-
-function getEmployeesCoverage(objeto) {
-  if (objeto === undefined) {
-    return todos();
-  }
-  const key = Object.keys(objeto);
-  const value = Object.values(objeto);
-  if (key[0] === 'name') {
-    const col = data.employees.find((e) => e.firstName === value[0] || e.lastName === value[0]);
-    return testeFunction(col.id, col.firstName, col.lastName, col.responsibleFor);
-  }
-  if (key[0] === 'id') {
-    if (value[0] === 'Id inválido') {
-      throw new Error('Informações inválidas');
-    }
-    const col = data.employees.find((e) => e.id === value[0]);
-    return testeFunction(col.id, col.firstName, col.lastName, col.responsibleFor);
-  }
-}
-
-console.log(getEmployeesCoverage());
-
-module.exports = getEmployeesCoverage;
-
-// functionLocation funcionando
-
-// const functionLocation = () => {
+// const data = require('../data/zoo_data');
+// // function species
+// const functionSpecies = (arr) => {
 //   const arrTeste = [];
-//   col.responsibleFor.forEach((e) => {
+//   arr.forEach((e) => {
 //     for (let i = 0; i < data.species.length; i += 1) {
-//       console.log(`Esse é meu e: ${e}`);
-//       console.log(`Esse é meu id: ${data.species[i].id}`);
+//       // console.log(`Esse é meu e: ${e}`);
+//       // console.log(`Esse é meu id: ${data.species[i].id}`);
+//       if (e === data.species[i].id) {
+//         arrTeste.push(data.species[i].name);
+//       }
+//     }
+//   });
+//   return arrTeste;
+// };
+
+// // função para retornar o location
+// const functionLocation = (arr) => {
+//   const arrTeste = [];
+//   arr.forEach((e) => {
+//     for (let i = 0; i < data.species.length; i += 1) {
+//       // console.log(`Esse é meu e: ${e}`);
+//       // console.log(`Esse é meu id: ${data.species[i].id}`);
 //       if (e === data.species[i].id) {
 //         arrTeste.push(data.species[i].location);
 //       }
@@ -92,12 +29,50 @@ module.exports = getEmployeesCoverage;
 //   return arrTeste;
 // };
 
-// FUNÇÃO FUNCIONANDO
+// // função para diminuir linhas
 // const testeFunction = (id, firstName, lastName, responsibleFor) => {
-//   return {
+//   const teste = {
 //     id,
 //     fullName: `${firstName} ${lastName}`,
-//     species: responsibleFor,
+//     species: functionSpecies(responsibleFor),
 //     locations: functionLocation(responsibleFor),
 //   };
+//   return teste;
 // };
+
+// //  função para todos
+// const todos = (responsibleFor) => {
+//   const arrTeste = [];
+//   data.employees.forEach((e) => {
+//     arrTeste.push({
+//       id: e.id,
+//       fullName: `${e.firstName} ${e.lastName}`,
+//       species: functionSpecies(e.responsibleFor),
+//       locations: functionLocation(e.responsibleFor),
+//     });
+//   });
+//   return arrTeste;
+// };
+
+// // function getEmployeesCoverage(objeto) {
+// //   if (objeto === undefined) {
+// //     return todos();
+// //   }
+// //   const key = Object.keys(objeto);
+// //   const value = Object.values(objeto);
+// //   if (key[0] === 'name') {
+// //     const col = data.employees.find((e) => e.firstName === value[0] || e.lastName === value[0]);
+// //     return testeFunction(col.id, col.firstName, col.lastName, col.responsibleFor);
+// //   }
+// //   if (key[0] === 'id') {
+// //     if (value[0] === 'Id inválido') {
+// //       throw new Error('Informações inválidas');
+// //     }
+// //     const col = data.employees.find((e) => e.id === value[0]);
+// //     return testeFunction(col.id, col.firstName, col.lastName, col.responsibleFor);
+// //   }
+// // }
+
+// // console.log(getEmployeesCoverage());
+
+// // module.exports = getEmployeesCoverage;
--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,30 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+describe('Testes da função getOpeningHours', () => {
+  it('Teste passando nenhum argumento', () => {
+    expect(getOpeningHours()).toEqual({
+      Tuesday: { open: 8, close: 6 },
+      Wednesday: { open: 8, close: 6 },
+      Thursday: { open: 10, close: 8 },
+      Friday: { open: 10, close: 8 },
+      Saturday: { open: 8, close: 10 },
+      Sunday: { open: 8, close: 8 },
+      Monday: { open: 0, close: 0 },
+    });
+  });
+  it('Teste com o argumento \'Monday\'', () => {
+    expect(getOpeningHours('Monday', '09:00-AM')).toBe('The zoo is closed');
+  });
+  it('Teste com o argumento \'Tuesday\'', () => {
+    expect(getOpeningHours('Tuesday', '09:00-AM')).toBe('The zoo is open');
+  });
+  it('Teste se o primeiro argumento não for um dia da semana', () => {
+    expect(() => getOpeningHours('Thu', '09:00-AM')).toThrow(/The day must be valid. Example: Monday/);
+  });
+  it('Teste se o segundo argumento não possui hora correta', () => {
+    expect(() => getOpeningHours('Saturday', 'C9:00-AM')).toThrow(/The hour should represent a number/);
+  });
+  it('Teste se o segundo argumento não possui AM ou PM correto', () => {
+    expect(() => getOpeningHours('Friday', '09:00-ZM')).toThrow('The abbreviation must be \'AM\' or \'PM\'');
+  });
+});
