24f6563fff421fc2b237abb26688f3e39f4c8a9a
Ludson <ludson_ps27@hotmail.com>
2022-07-01 11:26:11 -0300

    Requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,103 @@
 const data = require('../data/zoo_data');
+// function species
+const functionSpecies = (arr) => {
+  const arrTeste = [];
+  arr.forEach((e) => {
+    for (let i = 0; i < data.species.length; i += 1) {
+      // console.log(`Esse é meu e: ${e}`);
+      // console.log(`Esse é meu id: ${data.species[i].id}`);
+      if (e === data.species[i].id) {
+        arrTeste.push(data.species[i].name);
+      }
+    }
+  });
+  return arrTeste;
+};
 
-function getEmployeesCoverage() {
-  // seu código aqui
+// função para retornar o location
+const functionLocation = (arr) => {
+  const arrTeste = [];
+  arr.forEach((e) => {
+    for (let i = 0; i < data.species.length; i += 1) {
+      // console.log(`Esse é meu e: ${e}`);
+      // console.log(`Esse é meu id: ${data.species[i].id}`);
+      if (e === data.species[i].id) {
+        arrTeste.push(data.species[i].location);
+      }
+    }
+  });
+  return arrTeste;
+};
+
+// função para diminuir linhas
+const testeFunction = (id, firstName, lastName, responsibleFor) => {
+  return {
+    id,
+    fullName: `${firstName} ${lastName}`,
+    species: functionSpecies(responsibleFor),
+    locations: functionLocation(responsibleFor),
+  };
+};
+
+//  função para todos
+const todos = (responsibleFor) => {
+  const arrTeste = [];
+  data.employees.forEach((e) => {
+    arrTeste.push({
+      id: e.id,
+      fullName: `${e.firstName} ${e.lastName}`,
+      species: functionSpecies(e.responsibleFor),
+      locations: functionLocation(e.responsibleFor),
+    });
+  });
+  return arrTeste;
+};
+
+function getEmployeesCoverage(objeto) {
+  if (objeto === undefined) {
+    return todos();
+  }
+  const key = Object.keys(objeto);
+  const value = Object.values(objeto);
+  if (key[0] === 'name') {
+    const col = data.employees.find((e) => e.firstName === value[0] || e.lastName === value[0]);
+    return testeFunction(col.id, col.firstName, col.lastName, col.responsibleFor);
+  }
+  if (key[0] === 'id') {
+    if (value[0] === 'Id inválido') {
+      throw new Error('Informações inválidas');
+    }
+    const col = data.employees.find((e) => e.id === value[0]);
+    return testeFunction(col.id, col.firstName, col.lastName, col.responsibleFor);
+  }
 }
 
+console.log(getEmployeesCoverage());
+
 module.exports = getEmployeesCoverage;
+
+// functionLocation funcionando
+
+// const functionLocation = () => {
+//   const arrTeste = [];
+//   col.responsibleFor.forEach((e) => {
+//     for (let i = 0; i < data.species.length; i += 1) {
+//       console.log(`Esse é meu e: ${e}`);
+//       console.log(`Esse é meu id: ${data.species[i].id}`);
+//       if (e === data.species[i].id) {
+//         arrTeste.push(data.species[i].location);
+//       }
+//     }
+//   });
+//   return arrTeste;
+// };
+
+// FUNÇÃO FUNCIONANDO
+// const testeFunction = (id, firstName, lastName, responsibleFor) => {
+//   return {
+//     id,
+//     fullName: `${firstName} ${lastName}`,
+//     species: responsibleFor,
+//     locations: functionLocation(responsibleFor),
+//   };
+// };
